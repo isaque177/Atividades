aac141d8519f6faac66ec9a78b385054c965385f
isaque177 <0001083214@senaimgaluno.com.br>
2025-01-28 23:59:01 -0300

    Initial commit

--- src/revisao_java/atv1A.java
+++ src/revisao_java/atv1A.java
@@ -1,5 +1,14 @@
 package revisao_java;
-
+import java.util.Scanner;
 public class atv1A {
-
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite um número: "); int
+                num = scanner.nextInt();
+        if (num >= 0){
+            System.out.println("Este número é positivo");
+        }else {
+            System.out.println("Este número é negativo");
+        }
+    }
 }
