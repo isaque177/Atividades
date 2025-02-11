aa15196d05dd3b84431835dbe7d7b0f36f210d12
isaque177 <0001083214@senaimgaluno.com.br>
2025-02-11 19:49:09 -0300

    Delete expr_aritm_20.java

--- expr_aritm_20.java
+++
@@ -1,9 +0,0 @@
-package Revisão_math_e_expressões_numéricas;
-
-public class expr_aritm_20 {
-    public static void main(String[] args) {
-        int elev = (int) Math.pow(2,2);
-        int calculo = (10 + 5)/(elev);
-        System.out.println(calculo);
-    }
-}
