37b2c57209cb322dd1f5e5b870db312bb70fb405
isaque177 <0001083214@senaimgaluno.com.br>
2025-02-11 19:46:24 -0300

    Delete expr_aritm_10.java

--- expr_aritm_10.java
+++
@@ -1,9 +0,0 @@
-package Revisão_math_e_expressões_numéricas;
-
-public class expr_aritm_10 {
-    public static void main(String[] args) {
-
-        int calculo = (8 +2) * (6 -4);
-        System.out.println(calculo);
-    }
-}
