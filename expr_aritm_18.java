617226ba3903851dcc214f00a9b5123bde3e242b
isaque177 <0001083214@senaimgaluno.com.br>
2025-02-11 19:48:53 -0300

    Delete expr_aritm_18.java

--- expr_aritm_18.java
+++
@@ -1,9 +0,0 @@
-package Revisão_math_e_expressões_numéricas;
-
-public class expr_aritm_18 {
-    public static void main(String[] args) {
-
-        int calculo = (50/(5+5)) + 10 ;
-        System.out.println(calculo);
-    }
-}
