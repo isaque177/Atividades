174088b69c032c144662ca2751479fca6682b28c
isaque177 <0001083214@senaimgaluno.com.br>
2025-02-11 19:49:33 -0300

    Delete expr_aritm_5.java

--- expr_aritm_5.java
+++
@@ -1,9 +0,0 @@
-package Revisão_math_e_expressões_numéricas;
-
-public class expr_aritm_5 {
-    public static void main(String[] args) {
-
-        int calculo = 25 % 4;
-        System.out.println(calculo);
-    }
-}
