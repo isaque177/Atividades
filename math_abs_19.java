8c63a8895f370d005e8a59ad5fb304cce8dae966
isaque177 <0001083214@senaimgaluno.com.br>
2025-02-12 14:26:57 -0300

    Delete math_abs_19.java

--- math_abs_19.java
+++
@@ -1,9 +0,0 @@
-package Revisão_math_e_expressões_numéricas;
-
-public class math_abs_19 {
-    public static void main(String[] args) {
-
-        double abs = Math.abs((double) -8.8);
-        System.out.println(abs);
-    }
-}
