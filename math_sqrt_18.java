6b2f171abeb24c57607925a5f6dfb86c7636687c
isaque177 <0001083214@senaimgaluno.com.br>
2025-02-12 14:32:08 -0300

    Delete math_sqrt_18.java

--- math_sqrt_18.java
+++
@@ -1,9 +0,0 @@
-package Revisão_math_e_expressões_numéricas;
-
-public class math_sqrt_18 {
-    public static void main(String[] args) {
-
-        int raizq = (int) Math.sqrt(36);
-        System.out.println(raizq);
-    }
-}
