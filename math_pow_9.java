89e5a4f282f79a06eb7272bf5d0229bf8c76dc17
isaque177 <0001083214@senaimgaluno.com.br>
2025-02-12 14:30:46 -0300

    Delete math_pow_9.java

--- math_pow_9.java
+++
@@ -1,9 +0,0 @@
-package Revisão_math_e_expressões_numéricas;
-
-public class math_pow_9 {
-    public static void main(String[] args) {
-
-        int elev = (int) Math.pow(4,0.5);
-        System.out.println(elev);
-    }
-}
