c011bf8ea959abca0c3c4b135b797a5b8bb476fb
isaque177 <0001083214@senaimgaluno.com.br>
2025-02-12 14:31:25 -0300

    Delete math_sqrt_12.java

--- math_sqrt_12.java
+++
@@ -1,9 +0,0 @@
-package Revisão_math_e_expressões_numéricas;
-
-public class math_sqrt_12 {
-    public static void main(String[] args) {
-
-        int raizq = (int) Math.sqrt(121);
-        System.out.println(raizq);
-    }
-}
