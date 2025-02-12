6c14c71c50924b7a1244d3bdb3a1cf88284a07e9
isaque177 <0001083214@senaimgaluno.com.br>
2025-02-12 14:28:07 -0300

    Delete math_abs_9.java

--- math_abs_9.java
+++
@@ -1,9 +0,0 @@
-package Revisão_math_e_expressões_numéricas;
-
-public class math_abs_9 {
-    public static void main(String[] args) {
-
-        int abs = Math.abs((int)-999);
-        System.out.print(abs);
-    }
-}
